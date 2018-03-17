3916d182be25eeade81433a1278d8207a251c32d
George Witteman <georgewitteman@gmail.com>
2018-03-16 19:59:00 -0700

    Redo test runner

--- test.rkt
+++ test.rkt
@@ -1,56 +1,71 @@
 #lang racket
-(define (my-add a b) (+ a b))
+(define (my-add a b c) (+ a b c))
 
-(define (my-subtract a b) (+ a b))
+(define (my-subtract a b c) (+ a (- b c)))
 
-; c: the function that checks for equality of the return values
-; t: the function to test
-; s: the solution function
-; . args: the arguments to pass to both functions
-(define (assert c t s . args)
-  (c (apply t args) (apply s args)))
+;; A test case
+(struct case (myfn solnfn args))
 
-; t: the function to test
-; s: the solution function
-; args: a list of arguments to pass to both functions
-(define (assert-eq? t s args)
-  (apply assert (append (list eq? t s) args)))
+;; Result of a test case
+(struct result (case passed))
 
-; t: the function to test
-; s: the solution function
-; args: a list of lists of arguments to pass to the functions (test cases)
-; returns #t if all the tests passed, #f if at least one failed
-(define (test t s args)
-  (test-help t s args null null))
+;; Assert with CHECK that the two input functions, MYFN and SOLNFN,
+;; return the same value when applied to ARGS
+(define (assert check myfn solnfn args)
+  (check (apply myfn args) (apply solnfn args)))
 
-; t: the function to test
-; s: the solution function
-; args: a list of lists of arguments to pass to the functions (test cases)
-; passed: a list of the tests that passed
-; failed: a list of the tests that failed
-; returns #t if all the tests passed, #f if at least one failed
-(define (test-help t s args passed failed)
-  (cond
-    ;; No more test cases
-    [(empty? args) (print-summary passed failed)
-                   (= (length failed) 0)]
-    ;; Tests passed
-    [(assert-eq? t s (first args)) (test-help
-                                    t
-                                    s
-                                    (rest args)
-                                    (cons (first args) passed)
-                                    failed)]
-    ;; Tests failed
-    [else (test-help t s (rest args) passed (cons (first args) failed))]))
+;; Call ASSERT with CHECK equal to EQ?
+(define (assert-eq? myfn solnfn args)
+  (assert eq? myfn solnfn args))
 
-; passed: a list of the tests that passed
-; failed: a list of the tests that failed
-(define (print-summary passed failed)
-  (map (lambda (test) (printf "Passed test: ~a~n" test)) passed)
-  (map (lambda (test) (printf "Failed test: ~a~n" test)) failed)
-  (printf
-   "Finished running ~a tests. Passed: ~a. Failed ~a.~n"
-   (+ (length passed) (length failed))
-   (length passed)
-   (length failed)))
+;; Run a given test case
+(define (test a-case)
+  (result a-case (assert-eq? (case-myfn a-case) (case-solnfn a-case) (case-args a-case))))
+
+;; Run a list of test cases
+(define (test-suite tests)
+  (map test tests))
+
+;; Print results of test
+(define (print-results results)
+  (let* 
+      ;; Calculate number of passed and failed tests 
+      ([pass-fail
+        (foldl (lambda (a-result results)
+                 (cond
+                   ;; Passed test
+                   [(result-passed a-result)
+                    ;; Increment passed counter
+                    (list (+ 1 (first results)) (second results))]
+                   ;; Failed test
+                   [else
+                    (let* ([a-case (result-case a-result)]
+                           [myfn-name (object-name (case-myfn a-case))]
+                           [args (case-args a-case)])
+                      ;; Print info from test
+                      (printf "Failed: ~a ~a~n" myfn-name (case-args a-case))
+                      ;; Increment failed counter
+                      (list (first results) (+ 1 (second results))))]))
+               '(0 0)
+               results)]
+       ;; Calculate total number of tests
+       [total (+ (first pass-fail) (second pass-fail))]
+       [passed (first pass-fail)]
+       [failed (second pass-fail)]
+       [percent-correct (* (/ passed total) 100)])
+    ;; Print test suite summary
+    (printf "Ran ~a tests. ~a passed, ~a failed. Grade ~a%.~n"
+            total
+            passed
+            failed
+            percent-correct)))
+
+(print-results
+ (test-suite (list (case my-subtract - '(1 2 3))
+                   (case my-subtract - '(0 0 0))
+                   (case my-subtract - '(-1 1 -1))
+                   (case my-subtract - '(0 0 1))
+                   (case my-add + '(1 2 3))
+                   (case my-add + '(0 0 0))
+                   (case my-add + '(-1 1 -1))
+                   (case my-add + '(0 0 1)))))
