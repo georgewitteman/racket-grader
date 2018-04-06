a8b6f909e9314d7963819573c8bfba4e54a4a6eb
yinawangg <ziwang@vassar.edu>
2018-04-06 15:36:58 -0400

    Added test functions

---
+++ test-suite.bak
@@ -0,0 +1,5 @@
+(define test-suite
+  (list
+   (grade 'Q1' 1.5 add '(3 2) 5)
+   (grade 'Q1' 0.5 add '(0 0) 0)
+   (grade 'Q1' 2.0 add '(3 2) 2)))
--- test-suite.rkt
+++ test-suite.rkt
@@ -1,5 +1,13 @@
+(define division
+  (lambda (x y)
+    (/ x y)))
+
+(define half
+  (lambda (x)
+    (/ x 2)))
+
 (define test-suite
   (list
-   (grade 'Q1' 1.5 add '(3 2) 5)
-   (grade 'Q1' 0.5 add '(0 0) 0)
-   (grade 'Q1' 2.0 add '(3 2) 2)))
+   (grade 'Q1' 1.5 division '(3 2) 1.5)
+   (grade 'Q1' 0.5 division '(0 0) 0)
+   (grade 'Q1' 2.0 half '(2) 1)))
